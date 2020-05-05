27f6e2ef291aa3f3d939a7fd8d4464d0b285a9b2
Muhammad Fajri Rasid <fajrimuhammad460@gmail.com>
2020-05-05 10:39:47 +0800

    new update(5/5/2020)

--- Hacker Rank/Mutations.py
+++ Hacker Rank/Mutations.py
@@ -1,6 +1,4 @@
 # mutations
-
-
 def mutate_string(string, position, character):
     return string[:position] + character + string[position + 1 :]
 
--- Hacker Rank/StringValidator.py
+++ Hacker Rank/StringValidator.py
@@ -1,1 +1,35 @@
 # String validator
+def check(totalV):
+    if totalV != 0:
+        print(True)
+    else:
+        print(False)
+
+
+if __name__ == "__main__":
+    s = input()
+    totalValidator = 0
+    for i in s:
+        if i.isalnum():
+            totalValidator += 1
+    check(totalValidator)
+    totalValidator = 0
+    for i in s:
+        if i.isalpha():
+            totalValidator += 1
+    check(totalValidator)
+    totalValidator = 0
+    for i in s:
+        if i.isdigit():
+            totalValidator += 1
+    check(totalValidator)
+    totalValidator = 0
+    for i in s:
+        if i.islower():
+            totalValidator += 1
+    check(totalValidator)
+    totalValidator = 0
+    for i in s:
+        if i.isupper():
+            totalValidator += 1
+    check(totalValidator)
