72254b073f510966c643d434db2d2a99bc1b1a41
Muhammad Fajri Rasid <fajrimuhammad460@gmail.com>
2020-07-08 06:06:01 +0800

    new update (7/8/2020)

--- Hacker Rank/Test.py
+++ Hacker Rank/Test.py
@@ -1,6 +1,7 @@
 # for i in range(1, 5):
 #     # akan di print list dari index ke 1 sampai 4
 #     print([1, 2, 3, 4, 5][i])
+
 import itertools
 
 print(list(itertools.chain([1, 2, 3], [4, 5, 6])))
